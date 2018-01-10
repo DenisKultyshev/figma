af9c00e0da9d3e17e06e69aed28cc650edbd79ec
Sukhov Anton <littlegreenbagg@googlemail.com>
2018-01-10 14:21:03 +0300

    added animation, js

--- app/js/common.js
+++ app/js/common.js
@@ -48,10 +48,55 @@ $(document).ready(function() {
 
 $(function() {
 	$(document).pjax('a', '#pjax-container', {fragment: '#pjax-container'});
-	
+	$("header").animated("bounceInDown");
+	$("h1").animated("fadeInLeft");
+	$('.green-button').animated('fadeIn');
+	$('ul:not(footer ul)').animated('fadeInUp');
+	$('.item1').animated('fadeInRight');
+	$('.item2').animated('fadeInLeft');
+	$('.item3').animated('fadeInRight');
+	$('form').animated('zoomIn');
+	$('footer__list').animated('fadeIn');
+	$('h3').animated('fadeInLeft');
+	$('h2:not(.ourwork h2):not(.contact h2)').animated('fadeInRight');
+	$('.project-form div').animated('slideInUp');
+	$('.carousel').animated('zoomIn');
+	$('.service__item').animated('slideInUp');
+	$('.manager__shoe').animated('slideInUp');
+	$('.manager__text').animated('slideInUp');
+	$('.p-carousel').animated('slideInRight');
+	$('.preview__text').animated('slideInUp');
+	$('.project__img-wrapper').animated('slideInLeft');
 });
 
 $(document).ready(function() {
 	let height = document.documentElement.clientHeight;
 	$('.main').css('min-height', height);
+	const timeChilds = document.querySelectorAll('.time-sec__wrapper > *')
+	const crChilds = document.querySelectorAll('.create-sec__wrapper > *')
+	const respChilds = document.querySelectorAll('.resp-sec__wrapper > *')
+
+	respChilds.forEach(i => i.addEventListener('click', function() {
+		if (this.classList.contains('project-form__item--active')) {
+			this.classList.remove('project-form__item--active')
+		} else {
+			this.classList.add('project-form__item--active')
+		}
+	}))
+
+	const arRunner = (arr) => {
+		arr.forEach(i => i.addEventListener('click', function() {
+		if (this.classList.contains('project-form__item--active')) {
+			this.classList.remove('project-form__item--active')
+		} else {
+			for (let i = 0; i < this.parentElement.children.length; i++) {
+				this.parentElement.children[i].classList.remove('project-form__item--active')
+			}
+			this.classList.add('project-form__item--active')
+		}
+	}))
+	}
+
+	arRunner(timeChilds)
+	arRunner(crChilds)
 });
